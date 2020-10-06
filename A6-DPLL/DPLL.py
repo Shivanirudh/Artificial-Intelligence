6fcc8125f3ee3605066b5eb96df5971e8e5f6782
Shivanirudh <shivanirudh2001@gmail.com>
2020-10-06 15:36:19 +0530

    Skeleton complete

--- A6-DPLL/DPLL.py
+++ A6-DPLL/DPLL.py
@@ -4,11 +4,64 @@ class Clause():
     def __init__(self, no_literals, literals):
         self.no_literals = no_literals
         self.literals = [i for i in literals]
+        self.values = []
     
     def __str__(self):
-        print("{", end = "")
+        clause = "{ "
         for i in range(0, self.no_literals):
-            print(i, end = " ")
+            clause += (self.literals[i] + " ")
             if i != self.no_literals - 1:
-                print("v", end = " ")
-        print("}", end = "")
+                clause += ", "
+        clause += "}"
+        return clause
+    
+    def evaluate(self):
+
+class Formula():
+    def __init__(self, no_clauses, clauses):
+        self.no_clauses = no_clauses
+        self.clauses = [i for i in clauses]
+    
+    def __str__(self):
+        formula = "{ "
+        for i in range(0, self.no_clauses):
+            formula += (self.clauses[i] + " ")
+            if i != self.no_clauses - 1:
+                formula += ", "
+        formula += "}"
+        return formula
+
+def generate_clause():
+    literals = []
+    no_literals = random.randint(1, 5)
+    base_var = "A"
+    i = 0
+    while i < no_literals:
+        var_no = random.randint(1, 5)
+        var_name = base_var+str(var_no)
+        var_complement = random.randint(0, 1)
+        if var_complement == 1:
+            var_name += "'"
+        if var_name in literals:
+            i -= 1
+        else:
+            literals.append(var_name)
+        i+=1
+    clause = Clause(no_literals, literals)
+    return clause
+
+def generate_formula():
+    clauses = []
+    no_clauses = random.randint(1, 10)
+    i = 0
+    while i < no_clauses:
+        clause = generate_clause()
+        if clause in clauses:
+            i -= 1
+        else:
+            clauses.append(clause)
+        i += 1
+    formula = Formula(no_clauses, clauses)
+    return formula
+
+
